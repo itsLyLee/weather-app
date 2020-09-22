6828fa2adedfdf98fdf4a3b9285ea2b66e83d057
itsLyLee <lyylee00@gmail.com>
2020-09-22 16:13:24 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const path = require ("path")
 const express = require("express")
 const app = express()
+const router = require ("./router")
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
@@ -8,16 +9,7 @@ app.use(express.static("public"))
 app.set("views", "views")
 app.set("view engine", "hbs") 
 
-app.get("/", (req, res) => {
-   res.render("index")
-})
-
-
-
-app.get("/about", (req, res) => {
-    res.render("about")
-
-})
+app.use("/", router)
 
 app.listen(3000, () => {
     console.log("The server is now running on Port 3000")
