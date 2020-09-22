32d0146947d6970b3b4781c1ccfa3821e736ae2f
itsLyLee <lyylee00@gmail.com>
2020-09-22 14:09:54 +0200

    update

--- src/app.js
+++ src/app.js
@@ -2,20 +2,22 @@ const path = require ("path")
 const express = require("express")
 const app = express()
 
-
-//app.get("/", (req, res) => {
-//    res.send("My first expess web page!!!!")
-//})
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static("public"))
+app.set("views", "views")
+app.set("view engine", "hbs") 
 
 app.get("/", (req, res) => {
-    res.sendFile("index.html", {
-        root: path.join(__dirname, "../public")
-    })
+   res.send("My first expess web page!!!!")
 })
-// app.get("/about", (req, res) => {
-//     res.send("My business is so cool!!!")
 
-// })
+
+
+app.get("/about", (req, res) => {
+    res.send("My business is so cool!!!")
+
+})
 
 app.listen(3000, () => {
     console.log("The server is now running on Port 3000")
