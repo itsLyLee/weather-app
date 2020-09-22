89270a0dcf86bdecb5dd61fc3e3c769b923bd51a
itsLyLee <lyylee00@gmail.com>
2020-09-22 17:00:06 +0200

    update

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,9 +1,19 @@
+const axios = require("axios")
+const API_KEY = "920b55260aab534d9ad17f0fd61f485a"
+const url = `http://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}&units=metric`
+
 exports.renderHomePage = (req, res) => {
     res.render("index")
 }
 
 exports.getWeather = (req, res) => {
-    res.send("Your form has been submitted!")
+    const city = req.body.city
+    axios.get(url).then((response) => {
+        console.log(response)
+    }).catch((error) => {
+       console.log(error) 
+    })
+    
 }
 
 exports.renderAboutPage = (req, res) => {
