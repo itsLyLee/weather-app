9c24bfb7b3a5c795062734d828adb8e4d6054d78
itsLyLee <lyylee00@gmail.com>
2020-09-22 16:31:03 +0200

    update

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,6 +2,10 @@ exports.renderHomePage = (req, res) => {
     res.render("index")
 }
 
+exports.getWeather = (req, res) => {
+    res.send("Your form has been submitted!")
+}
+
 exports.renderAboutPage = (req, res) => {
     res.render("about")
 }
